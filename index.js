4b3b4853ed128928770edec64a718d78bdba8867
Xhram <87285044+Xhram@users.noreply.github.com>
2024-04-11 14:38:41 -0500

    Added response.properEnd and response.hasEnded

--- index.js
+++ index.js
@@ -3,6 +3,19 @@ const http = require("http");
 var port = 8080
 
 function requestListener(request,response){
+
+    response.hasEnded = false;
+    response.properEnd = (...args) => {
+        if(response.hasEnded == false){
+            response.end(...args);
+            response.hasEnded = true;
+        }
+    }
+
+
+
+
+
     response.setHeader('Access-Control-Allow-Origin', '*');
     response.setHeader('Access-Control-Request-Method', '*');
     response.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST');
@@ -12,7 +25,7 @@ function requestListener(request,response){
 
     if(request.method == "OPTIONS"){
         console.log("Preflight Options Http Request")
-        response.end()
+        response.properEnd()
         return
     } else if (request.method == "POST"){
         console.log("Post Request")
@@ -30,10 +43,10 @@ function requestListener(request,response){
                     handleApiCall(data,response)
                 } catch (error){
                     response.write("<h1>Server Has Had an Internal Error</h1>")
-                    response.end();
+                    response.properEnd();
 
                 }
-                
+
                 
             }
             request.on("end", EndDataReviving);
@@ -42,7 +55,7 @@ function requestListener(request,response){
 
         } else {
             response.write("<h1>invalid Post Request: Please Forward Any Post Request To /api")
-            response.end()
+            response.properEnd()
             return
         }
         //add later
@@ -59,7 +72,8 @@ function requestListener(request,response){
 
 
     response.write("<h1>fall back code</h1>")
-    response.end()
+    response.properEnd()
+
 }
 
 var server = http.createServer(requestListener)
