a4c064e59d371b43de7691e22faacb2008bab81e
Xhram <87285044+Xhram@users.noreply.github.com>
2024-04-11 14:12:43 -0500

    Added method types handlening

--- index.js
+++ index.js
@@ -5,8 +5,25 @@ var port = 8080
 function requestListener(request,response){
     response.setHeader('Access-Control-Allow-Origin', '*');
     response.setHeader('Access-Control-Request-Method', '*');
-    response.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
+    response.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST');
     response.setHeader('Access-Control-Allow-Headers', '*');
+
+    if(request.method == "OPTIONS"){
+        response.end()
+        return
+    } else if (request.method == "POST"){
+        //add later
+    } else if(request.method == "GET"){
+        //add later
+    }
+
+
+
+
+
+
+
+
 
     response.write("<h1>server is on</h1>")
     response.end()
