0f764bb758eca6e8c72cce3234e4dd3b3d109c70
Xhram <87285044+Xhram@users.noreply.github.com>
2024-04-11 14:34:27 -0500

    Added Processing for Post Requests

--- index.js
+++ index.js
@@ -8,10 +8,43 @@ function requestListener(request,response){
     response.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST');
     response.setHeader('Access-Control-Allow-Headers', '*');
 
+    
+
     if(request.method == "OPTIONS"){
+        console.log("Preflight Options Http Request")
         response.end()
         return
     } else if (request.method == "POST"){
+        console.log("Post Request")
+        if(request.url.startsWidth("/api")){
+            //Get POST Data
+
+            let body = "";
+            var ReciveData = (chunk) =>{
+                body += chunk.toString();
+            }
+            request.on("data", ReciveData);
+
+            var EndDataReviving = () =>{
+                try {
+                    handleApiCall(data,response)
+                } catch (error){
+                    response.write("<h1>Server Has Had an Internal Error</h1>")
+                    response.end();
+
+                }
+                
+                
+            }
+            request.on("end", EndDataReviving);
+
+
+
+        } else {
+            response.write("<h1>invalid Post Request: Please Forward Any Post Request To /api")
+            response.end()
+            return
+        }
         //add later
     } else if(request.method == "GET"){
         //add later
@@ -25,7 +58,7 @@ function requestListener(request,response){
 
 
 
-    response.write("<h1>server is on</h1>")
+    response.write("<h1>fall back code</h1>")
     response.end()
 }
 
@@ -33,3 +66,12 @@ var server = http.createServer(requestListener)
 
 
 server.listen(port);
+
+
+function handleApiCall(data,response){
+
+
+
+}
+
+
