6a307a7c69acf305122d1c7993a52a727b2de47d
Xhram <87285044+Xhram@users.noreply.github.com>
2024-04-12 23:29:13 -0500

    Added generateToken function

--- index.js
+++ index.js
@@ -210,5 +210,13 @@ function handleApiCall(data,response){
 
 
 
-
+function generateToken() {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let randomString = '';
+    for (let i = 0; i < 16; i++) {
+      const randomIndex = Math.floor(Math.random() * characters.length);
+      randomString += characters[randomIndex];
+    }
+    return randomString;
+  }
 
